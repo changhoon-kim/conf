a5d78e26914216191c9fbb8ebeeb801edfec0d99
changhoon-kim <changhoon.kim@worksmobile.com>
2017-07-15 23:29:07 +0900

    add set link script

---
+++ set-link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -nfs ./.aliases ~/.aliases
+ln -nfs ./.bash_profile ~/.bash_profile # mac
+ln -nfs ./.bash_prompt ~/.bash_prompt
+ln -nfs ./.bashrc ~/.bashrc # ncloud
+ln -nfs ./.ctags ~/.ctags
